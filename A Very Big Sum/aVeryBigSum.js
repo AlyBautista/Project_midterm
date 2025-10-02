26aaf6058a848fc58fc5e3682a5b4a14b03cc43c
AlyBautista <bautistaalyssar16@gmail.com>
2025-10-03 00:46:43 +0800

    aVeryBigSum.js

--- A Very Big Sum/aVeryBigSum.js
+++ A Very Big Sum/aVeryBigSum.js
@@ -4,7 +4,4 @@ function aVeryBigSum(ar) {
         sum += ar[i];
     }
     return sum;
-}
-// Example test
-let ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
-let result = aVeryBigSum(ar);
+}
